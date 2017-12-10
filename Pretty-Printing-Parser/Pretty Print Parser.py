8b9864423005a2db9981436f46bb93033afcd45b
Vijayadeepak <subramanian.a@outlook.com>
2017-12-09 20:52:48 -0600

    Working code

--- Pretty-Printing-Parser/Pretty Print Parser.py
+++ Pretty-Printing-Parser/Pretty Print Parser.py
@@ -1,39 +1,123 @@
 import sys
 import json
+import pdb
 
-def scanIDs (array) :
-    sample_dict = {}
-    temp = array[0].split(' ')
-    sample_dict['action'] = temp[0]
-    sample_dict ['type'] = temp[1][:temp[1].find('(')]
-    sample_dict['subtype'] = temp[1][temp[1].find('(')+1:temp[1].find(')')]
-    #sample_dict['data'] = dict(array[2].split(' '))
-    print(array[2][array[2].find('-'):].split(' '))
-    return sample_dict
+class Parser:
+    state = ''
+    substate = ''
+    action = []
+    Type = []
+    key = []
+    value = []
+    subtype = []
+    data = {}
+    curlystack = []
+    stack = []
+    output = {}
+    threshold = 1
+    def initialize (self):
+        self.state = 'action'
+    def reset (self):
+        self.state = ''
+        self.substate = ''
+        self.action = []
+        self.Type = []
+        self.key = []
+        self.value = []
+        self.subtype = []
+        self.data = {}
+        self.curlystack = []
+        self.stack = []
+        self.threshold = 1
 
-def extractBlock (FileData):
-    array = []
-    for lines in FileData:
-        array.append(lines.lstrip('\t').rstrip('\n'))
-        if (lines.find(';') >= 0 ) :
-            break
-    return array
+    def updatedict (self, hashmap, key, value):
+        #print ('%s %s' %(key,value))
+        if key in hashmap:
+            if type(hashmap[key]) is not type([]):
+                hashmap[key] = [hashmap[key]]
+                hashmap[key].append(value)
+            else:
+                hashmap[key].append(value)
+        else:
+            hashmap[key] = value
 
-#def GenHashmap (array):
+    def processline(self,line):
+        if self.state == 'action' :
+            if line == ' ':
+                self.state = 'type'
+            elif line != '\n':
+                self.action.append(line)
+        elif self.state == 'type' :
+            if line == '(':
+                self.stack.append(line)
+                self.state = 'subtype'
+            else:
+                self.Type.append(line)
+        elif self.state == 'subtype' :
+            if line ==')':
+                if (''.join(self.subtype) == 'cassette'):
+                    self.threshold = 2
+                else:
+                    self.threshold = 1
+            if line == '{':
+                self.curlystack.append(line)
+                if len(self.curlystack) >= self.threshold:
+                    self.state = 'data'
+                    self.substate = 'key'
+            elif line != ')' and line != '\n':
+                self.subtype.append(line)
+        elif self.state == 'data' :
+            #print(''.join(self.key),''.join(self.value))
+            if self.substate == 'key':
+                if line == ' ' and len(self.key) != 0:
+                    self.substate = 'value'
+                elif "\n" not in line:
+                    if line!= '-' and line != ' ':
+                        if line == '}':
+                            if len(self.curlystack) != 0:
+                                self.curlystack.pop()
+                            self.key = []
+                            self.value = []
+                        elif line == ';':
+                            #pdb.set_trace()
+                            self.updatedict(self.output,'action',''.join(self.action))
+                            self.updatedict(self.output,'type',''.join(self.Type))
+                            self.updatedict(self.output,'subtype',''.join(self.subtype))
+                            self.updatedict(self.output,'data',self.data)
+                            self.reset()
+                            self.state = 'action'
+                        elif line != '\t' and line != '{':
+                            self.key.append(line)
 
-FileData = open ('sample.txt', 'r')
+            elif self.substate == 'value':
+                #pdb.set_trace()
+                if line == '{':
+                    self.curlystack.append(line)
+                elif line == '}' and len(self.curlystack) != 0:
+                    self.curlystack.pop()
+                elif line == '}' and len(self.curlystack) < self.threshold:
+                    self.curlystack.pop()
+                    #self.data[str(''.join(self.key))] = str(''.join(self.value))
+                    self.updatedict(self.data,str(''.join(self.key)),str(''.join(self.value)))
+                    self.key = []
+                    self.value = []
+                elif line == '\n':
+                    #self.data[str(''.join(self.key))] = str(''.join(self.value))
+                    self.updatedict(self.data,str(''.join(self.key)),str(''.join(self.value)))
+                    self.key = []
+                    self.value = []
+                    self.substate = 'key'
+                else:
+                    self.value.append(line)
 
-temp = extractBlock(FileData)
-print (temp)
-print (scanIDs (temp))
-temp = extractBlock(FileData)
-print (temp)
-
-#json_str = json.dumps(array)
-#print(json.dumps(json_str, indent=4))
-#print (json_str)
-
-'''with open('data.json','w') as outfile:
-    json.dump(json_str, outfile)
-
-print(json.dumps(json_str, indent=4))'''
+if __name__ == '__main__' :
+    FileData = open ('sample.txt', 'r')
+    test = Parser()
+    test.initialize()
+    line = FileData.readline(1)
+    while (line):
+        test.processline(line)
+        line = FileData.readline(1)
+    print (test.output)
+    with open('data.json','w') as outfile:
+        json.dump(test.output, outfile)
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Pretty-Printing-Parser
+
+This is a sample program to convert a log file into a system readable JSON format file as given in the example.
+
+et machineStatus(cassette) {
+{
+  -name {P430_BLK}
+  -mfgDate {Sun Aug 7 17:27:16 2016}
+  -mfgLot {101275}
+  -initialMatl 56.3
+  -1stUsageDate {Fri Dec 16 09:50:45 2016}
+
+  -serialNumber {337213871.000000}
+  -remainingMatl 12.7353
+  -status Loaded
+}
+
+Pretty-printing parser output:
+
+[{'action': 'set',
+'data': {'1stUsageDate': 'Fri Dec 16 09:50:45 2016',
+         'initialMatl': 56.3,
+         'mfgDate': 'Sun Aug 7 17:27:16 2016',
+         'name': 'P430_BLK',
+         'remainingMatl': 12.7353,
+         'serialNumber': '337213871.000000',
+         'status': 'Loaded'},
+ 'subtype': 'cassette',
+ 'type': 'machineStatus’}]
